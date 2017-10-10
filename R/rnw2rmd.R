cc0aa783b4059c7adfe97e18488f5aa080e96919
unDocUMeantIt <m@reaktanz.de>
2017-10-10 16:09:29 +0200

    further work on rnw2rmd()

--- R/rnw2rmd.R
+++ R/rnw2rmd.R
@@ -21,16 +21,57 @@
 #' It tries its best to convert old Sweave vignettes into R Markdown.
 #' Please do not expect 
 #' 
-#' @params file Path to an *.Rnw file to convert.
+#' @param file Path to an *.Rnw file to convert.
+#' @param output Character string defining the R markdown output format.
+#' @param engine Character string defining the \code{VignetteEngine} value.
 #' @value A character string with the converted text. No file will be written.
 #' @export
 #' @references
 #'  [1] https://gist.github.com/mikelove/5618f935ace6e389d3fbac03224860cd
 #'  [2] https://gist.github.com/lgatto/d9d0e3afcc0a4417e5084e5ca46a4d9e
-rnw2rmd <- function(file){
-  txt <- readLines(file)
+rnw2rmd <- function(file, output="rmarkdown::html_vignette", engine="knitr::rmarkdown"){
+  txt <- txt_body <- readLines(file)
+
+  title <- which(grepl("\\\\title{", txt, perl=TRUE))
+  author <- which(grepl("\\\\author{", txt, perl=TRUE))
+  vignette_meta <- which(grepl("%\\\\Vignette.+", txt, perl=TRUE))
+  vignette_engine <- which(grepl("%\\\\VignetteEngine{.+", txt, perl=TRUE))
+  # remove engine if set
+  vignette_meta <- vignette_meta[!vignette_meta %in% vignette_engine]
+  usepackage <- which(grepl("\\\\usepackage", txt, perl=TRUE))
+
+  begin_document <- which(grepl("\\\\begin{document}", txt, perl=TRUE))
+  end_document <- which(grepl("\\\\end{document}", txt, perl=TRUE))
+  if(isTRUE(begin_document > 0)){
+    txt_body <- txt[c((begin_document + 1):ifelse(isTRUE(end_document > 0), (end_document-1), length(txt)))]
+  } else {}
+
+  preamble <- list()
+  if(isTRUE(title > 0)){
+    preamble[["title"]] <- gsub("\\\\title{(.+?)}", "\"\\1\"", txt[[title]], perl=TRUE)
+  } else {}
+  if(isTRUE(author > 0)){
+    preamble[["author"]] <- gsub("\\\\author{(.+?)}", "\"\\1\"", txt[[author]], perl=TRUE)
+  } else {}
+  preamble[["date"]] <- "`r Sys.Date()`"
+  preamble[["output"]] <- output
+  preamble[["vignette"]] <- paste(
+    ">",
+    paste0(txt[vignette_meta], collapse="\n  "),
+    paste0("%\\VignetteEngine{", engine, "}"),
+    paste0(txt[usepackage], collapse="\n  "),
+    sep="\n  "
+  )
+#   if(isTRUE( > 0)){
+#   } else {}
+  preamble_rmd <- "---"
+  for (thisPreamble in names(preamble)){
+    preamble_rmd <- paste0(preamble_rmd, "\n", thisPreamble, ": ", preamble[[thisPreamble]])
+  }
+  preamble_rmd <- paste0(preamble_rmd, "\n---\n")
 
   pat <- list(
+    c(from="\\\\maketitle", to=""),
     c(from="\\\\Robject{(.+?)}", to="`\\1`"),
     c(from="\\\\Rcode{(.+?)}", to="`\\1`"),
     c(from="\\\\Rclass{(.+?)}", to="*\\1*"),
@@ -50,15 +91,22 @@ rnw2rmd <- function(file){
     c(from="\\\\Biocpkg{(.+?)}", to="`r Biocpkg(\"\\1\")`"),
     c(from="\\\\cite{(.+?)}", to="[\\@\\1]"),
     c(from="\\\\ref{(.+?)}", to="\\\\@ref(\\1)"),
-   c(from="\\\\url{(.+?)}", to="<\\1>"),
+    c(from="\\\\url{(.+?)}", to="<\\1>"),
     c(from="\\\\ldots", to="\\.\\.\\."),
     c(from="\\\\label{", to=" {#"), ## only for sections
     c(from="\\\\deseqtwo{}", to="DESeq2")
   )
 
   for (thisPat in pat){
-    txt <- gsub(thisPat[["from"]], thisPat[["to"]], txt, perl=TRUE)
+    txt_body <- gsub(thisPat[["from"]], thisPat[["to"]], txt_body, perl=TRUE)
   }
   
-  return(txt)
+  txt_body <- paste0(preamble_rmd, paste0(txt_body, collapse="\n"))
+#   # replace Schunk environments
+#   txt_body <- gsub("\\\\begin{Schunk}(.*?)\\\\end{Schunk}", "\\1", txt_body, perl=TRUE)
+#   txt_body <- gsub("\\\\begin{Soutput}.*?\\\\end{Soutput}", "", txt_body, perl=TRUE)
+#   txt_body <- gsub("\\\\begin{Sinput}(.*?)\\\\end{Sinput}", "```{r}\n\\1\n```", txt_body, perl=TRUE)
+  
+  
+  return(txt_body)
 }
